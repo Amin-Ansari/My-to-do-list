7ac6a7055e5ef06902ef83b4baaba94a344bbbff
Amin Ansari <devansari@yahoo.com>
2023-03-11 20:16:21 +0330

    The AppContainer component is added to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import AppContainer from "./Components/UI/AppContainer";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <AppContainer></AppContainer>
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/Components/UI/AppContainer.js
@@ -0,0 +1,7 @@
+import classes from "./AppContainer.module.css";
+
+const AppContainer = () => {
+  return <div className={classes["app-container-style"]}></div>;
+};
+
+export default AppContainer;
