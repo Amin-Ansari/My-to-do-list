044f45addb074c4cb8583dab1c651ff92c09ae7c
Amin Ansari <devansari@yahoo.com>
2023-03-12 17:42:37 +0330

    The adding-topic is added to the project

--- src/Components/Data/AddButton.js
+++ src/Components/Data/AddButton.js
@@ -2,25 +2,42 @@ import "./AddButton.css";
 import ReactDom from "react-dom";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const AddingTopic = () => {
-  return (
-    <div className="topic-container">
-      <div>Add task</div>
-      <div>Add list</div>
-      <div>Add routine</div>
-    </div>
-  );
-};
+import { useState } from "react";
+
 const AddButton = () => {
+  const [isTopicAppear, updateAppear] = useState(false);
+  const toggleTopicContainer = () => {
+    isTopicAppear ? updateAppear(false) : updateAppear(true);
+  };
   return (
-    <button type="button" className="add-button-style">
+    <button
+      type="button"
+      className="add-button-style"
+      onClick={toggleTopicContainer}
+    >
       <FontAwesomeIcon icon="fa-solid fa-plus" />
       {ReactDom.createPortal(
-        <AddingTopic />,
+        <AddingTopic appearOrNot={isTopicAppear} />,
         document.getElementById("where-adding-topic-goes-to")
       )}
     </button>
   );
 };
 
+const AddingTopic = (props) => {
+  console.log(props.appearOrNot);
+  return (
+    <div className={`backdrop-shadow ${props.appearOrNot ? "appear" : "hide"}`}>
+      <div
+        className={`topic-container ${
+          props.appearOrNot ? "topict-goes-up" : "hide"
+        }`}
+      >
+        <div>Add task</div>
+        <div>Add list</div>
+        <div>Add routine</div>
+      </div>
+    </div>
+  );
+};
 export default AddButton;
