9b5797df113958630e4b2cfbbe3911e4df5a304e
Amin Ansari <devansari@yahoo.com>
2023-03-15 17:17:22 +0330

    The clock arrows are fixed (I guess)

--- src/Components/Data/Clock.js
+++ src/Components/Data/Clock.js
@@ -1,9 +1,54 @@
 import "./Clock.css";
 import ClockSVG from "../../images/clock-image.svg";
+import { useState } from "react";
+
 const Clock = () => {
+  const initialTime = new Date();
+  const [clockTime, updateClockTime] = useState({
+    hour:
+      initialTime.getHours() <= 9
+        ? `0${initialTime.getHours()}`
+        : initialTime.getHours(),
+    minute:
+      initialTime.getMinutes() <= 9
+        ? `0${initialTime.getMinutes()}`
+        : initialTime.getMinutes(),
+  });
+  setInterval(() => {
+    const newerTime = new Date();
+    updateClockTime({
+      hour:
+        newerTime.getHours() <= 9
+          ? `0${newerTime.getHours()}`
+          : newerTime.getHours(),
+      minute:
+        newerTime.getMinutes() <= 9
+          ? `0${newerTime.getMinutes()}`
+          : newerTime.getMinutes(),
+    });
+  }, 1000);
   return (
     <div className="image-wrapper">
-      <img src={ClockSVG} className="clock-image" />
+      <img src={ClockSVG} className="clock-image"></img>
+      <img
+        src={require("../../images/arrow.png")}
+        className="arrow hour-arrow"
+        style={{
+          transform: ` translate(-50%, -50%) rotate(${
+            Number(clockTime.hour) * 30 + Number(clockTime.hour / 6)
+          }deg)`,
+        }}
+      />
+      <img
+        src={require("../../images/arrow.png")}
+        className="arrow"
+        style={{
+          transform: ` translate(-50%, -50%) rotate(${
+            Number(clockTime.minute) * 6
+          }deg)`,
+        }}
+      />
+      <span className="arrow-dot"></span>
     </div>
   );
 };
