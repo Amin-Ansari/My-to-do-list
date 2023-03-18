42cb623baeff15977ae8c1b9dcf5c8d3e4fd7432
Amin Ansari <devansari@yahoo.com>
2023-03-18 21:32:51 +0330

    The UL and the list title are added and also the color is changed

--- src/Components/Data/ToDoList.js
+++ src/Components/Data/ToDoList.js
@@ -1,9 +1,11 @@
 import classes from "./ToDoList.module.css";
+import ListLayout from "../UI/ListLayout";
 
 export default function ToDoList() {
   return (
     <div className={classes["todo-list-style"]}>
       <h2 className={classes["title-style"]}>Tasks</h2>
+      <ListLayout></ListLayout>
     </div>
   );
 }
---
+++ src/Components/UI/ListLayout.js
@@ -0,0 +1,11 @@
+import "./ListLayout.css";
+
+export default function ListLayout() {
+  return (
+    <ul>
+      <div className="list-title">
+        <h4>Today</h4>
+      </div>
+    </ul>
+  );
+}
