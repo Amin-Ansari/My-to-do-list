456ea683b20920153617938a70990b24c28a79b1
Amin Ansari <devansari@yahoo.com>
2023-03-21 18:58:45 +0330

    The task adding button got fixed

--- src/Components/Data/TaskAddingButton.js
+++ src/Components/Data/TaskAddingButton.js
@@ -2,9 +2,11 @@ import "./TaskAddingButton.css";
 
 const TaskAddingButton = (props) => {
   return (
-    <button type="submit" className="task-adding-button">
-      Add
-    </button>
+    <div className="task-adding-button-container">
+      <button type="submit" className="task-adding-button">
+        Add
+      </button>
+    </div>
   );
 };
 
