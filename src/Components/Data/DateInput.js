e9c88c5b947784e17167c670ce44cbd0be876039
Amin Ansari <devansari@yahoo.com>
2023-03-22 17:17:05 +0430

    The DatePicker component is able to be closed and opened

--- src/Components/Data/DateInput.js
+++ src/Components/Data/DateInput.js
@@ -5,24 +5,42 @@ import ReactDOM from "react-dom";
 import { useState } from "react";
 import SelectButton from "./SelectButton";
 const DatePicker = (props) => {
+  function closeBackDrop() {
+    props.onClosingDPicker(false);
+  }
+  const monthChange = () => {
+    props.onClosingDPicker(true);
+  };
   return (
-    <Backdrop className={`backdrop-shadow ${true ? "appear" : "hide"}`}>
-      <div
-        className={`topic-container date-picker-padding ${
-          true ? "topict-goes-up" : "hide"
-        }`}
+    <>
+      <Backdrop
+        className={`backdrop-shadow ${props.hideOrShow ? "appear" : "hide"}`}
       >
-        <div className="month-control-wrapper">
-          <button>Previous month</button>
-          <button>Next month</button>
+        <div className="close-part" onClick={closeBackDrop}></div>
+        <div
+          className={`topic-container date-picker-padding ${
+            true ? "topict-goes-up" : "hide"
+          }`}
+        >
+          <div className="month-control-wrapper">
+            <button onClick={monthChange}>Previous month</button>
+            <button onClick={monthChange}>Next month</button>
+          </div>
+          <SelectButton></SelectButton>
         </div>
-        <SelectButton></SelectButton>
-      </div>
-    </Backdrop>
+      </Backdrop>
+    </>
   );
 };
 
 const DateInput = (props) => {
+  const [dateShowState, updateDateShowState] = useState(false);
+  const changeTheState = (stateValue) => {
+    updateDateShowState(stateValue);
+  };
+  const showTheDatePicker = () => {
+    updateDateShowState(true);
+  };
   return (
     <>
       <div className="input-wrapper">
@@ -33,11 +51,15 @@ const DateInput = (props) => {
             readOnly
             className="input-style hover-cursor"
             value={"02/20/2023"}
+            onClick={showTheDatePicker}
           ></input>
         </label>
       </div>
       {ReactDOM.createPortal(
-        <DatePicker />,
+        <DatePicker
+          hideOrShow={dateShowState}
+          onClosingDPicker={changeTheState}
+        />,
         document.getElementById("where-date-picker-goes-to")
       )}
     </>
